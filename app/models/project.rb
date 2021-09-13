cfdabf9265c7730f71cab0025f97d567b64dfcb9
benjamin <benjamin.olmsted@gmail.com>
2021-09-13 13:50:19 -0700

    added polymorphic realtions

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
     belongs_to :user
     has_many :project_widgets
+   # has_many :widgetables, :through => :project_widgets
+   def widgets
+    project_widgets.map { |pw| pw.widgetable}
+   end
 end
--- app/models/random_image_widget.rb
+++ app/models/random_image_widget.rb
@@ -1,3 +1,5 @@
 class RandomImageWidget < ApplicationRecord
     has_many :project_widgets, as: :widgetable
+
+    #has_many :projects, :through => :project_widgets, :as => :widgetable
 end
