98c556160cf2e0f8cf592419085d1322edd4cf9e
benjamin <benjamin.olmsted@gmail.com>
2021-09-16 09:31:14 -0700

    adds a random image to widget on add

--- client/src/components/RandomImageWidget.js
+++ client/src/components/RandomImageWidget.js
@@ -1,9 +1,14 @@
+import { Button } from "@material-ui/core"
+import ImageIcon from "@material-ui/icons/Image"
 
-
-function RandomImageWidget ({ image_url }){
+function RandomImageWidget ({ image_url, widget_id }){
 
     return (
-        <img src={image_url} width ="250px"></img>
+        <>
+            <img src={image_url} width ="250px"></img>
+            <Button startIcon={<ImageIcon />}
+                    >new image</Button>
+        </>
     )
 }
 
