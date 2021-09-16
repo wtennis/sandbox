37fe1886241fb7df5b53b96cb59d5ec7b395cf9d
benjamin <benjamin.olmsted@gmail.com>
2021-09-16 11:31:14 -0700

    removed some irrelevant things

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -15,18 +15,6 @@ import Container from '@material-ui/core/Container';
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -144,23 +132,9 @@ export default function SignUp({ setUser }) {
           >
             Sign Up
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+    
     </Container>
   );
 }
